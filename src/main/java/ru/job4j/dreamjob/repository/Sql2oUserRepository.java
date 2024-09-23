435418a9a70470d3b9d78f462c511694066f26ec
andrey <avbaranov@internet.ru>
2024-09-23 15:18:17 +1000

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -0,0 +1,68 @@
+package ru.job4j.dreamjob.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import ru.job4j.dreamjob.model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Sql2oUserRepository.class);
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        try (var connection = sql2o.open()) {
+            var sql = """
+                      INSERT INTO users(name, email, password)
+                      VALUES (:name, :email, :password)
+                      """;
+            var query = connection.createQuery(sql, true)
+                    .addParameter("name", user.getName())
+                    .addParameter("email", user.getEmail())
+                    .addParameter("password", user.getPassword());
+            int generatedKey = query.executeUpdate().getKey(Integer.class);
+            user.setId(generatedKey);
+        } catch (Exception e) {
+            LOGGER.error("Catching exception", e);
+            return Optional.empty();
+        }
+        return Optional.of(user);
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var sql = "SELECT * FROM users WHERE email = :email AND password = :password";
+            var query = connection.createQuery(sql, true)
+                    .addParameter("email", email)
+                    .addParameter("password", password);
+            var user = query.executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+    }
+
+    public Collection<User> findAll() {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT *FROM users");
+            return query.executeAndFetch(User.class);
+        }
+    }
+
+    public boolean deleteById(int id) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("DELETE FROM users WHERE id = :id")
+                    .addParameter("id", id);
+            var affectedRows = query.executeUpdate().getResult();
+            return affectedRows > 0;
+        }
+    }
+}
