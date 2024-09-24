07b49d46618c5262809baa1f3a29095ae796e917
andrey <avbaranov@internet.ru>
2024-09-25 09:48:23 +1000

    2. HttpSession [#504864]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.dreamjob.dto.FileDto;
 import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.service.CandidateService;
 import ru.job4j.dreamjob.service.CityService;
 
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
 
 @ThreadSafe
 @Controller
@@ -25,15 +26,26 @@ public class CandidateController {
     }
 
     @GetMapping
-    public String getAll(Model model) {
+    public String getAll(Model model, HttpSession session) {
+        setSessionAttribute(model, session);
         model.addAttribute("candidates", candidateService.findAll());
         return "candidates/list";
     }
 
     @GetMapping("/create")
-    public String getCreationPage(Model model) {
+    public String getCreationPage(Model model, HttpSession session) {
+        setSessionAttribute(model, session);
         model.addAttribute("cities", cityService.findAll());
         return "candidates/create";
+    }
+
+    private void setSessionAttribute(Model model, HttpSession session) {
+        var user = (User) session.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            user.setName("Гость");
+        }
+        model.addAttribute("user", user);
     }
 
     @PostMapping("/create")
